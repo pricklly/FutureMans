f474675a371d658d805a6da205e3925aa5763967
Сергей <36310292+pricklly@users.noreply.github.com>
2023-01-31 14:13:34 +0300

    Uploding
    if uploding NOW

---
+++ In- Future/unit1.pas
@@ -0,0 +1,51 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls, ComCtrls, Menus;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    CheckBox3: TCheckBox;
+    ComboBox1: TComboBox;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Image1: TImage;
+    Image2: TImage;
+    Image3: TImage;
+    Image4: TImage;
+    Label1: TLabel;
+    ListBox1: TListBox;
+    Memo1: TMemo;
+    Memo2: TMemo;
+    RadioButton1: TRadioButton;
+    TrackBar1: TTrackBar;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
